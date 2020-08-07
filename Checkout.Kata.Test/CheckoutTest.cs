616cf566b215694936433448df0aa70fbeb1aba5
Michael Greenhalgh <wordontheworld@gmail.com>
2020-08-07 12:55:03 +0100

    Initial Commit with Test

---
+++ Checkout.Kata.Test/CheckoutTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace Checkout.Kata.Test
+{
+    public class CheckoutTest
+    {
+        [Fact]
+        public void ItemScannedShowsInBasketTotal()
+        {
+            //Arrange
+            var checkout = new Checkout();
+            var item = new Item();
+
+            //Act
+            checkout.Scan(item);
+
+            var result = checkout.Basket.Count;
+
+            //Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ Checkout.Kata/Checkout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Checkout.Kata
+{
+    public class Checkout
+    {
+        public Checkout()
+        {
+        }
+
+        public List<Item> Basket { get; set; }
+
+        public void Scan(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
